1b90ce90d151330b1cd6dac21e0566abb6bae2aa
Sergio J Garcia <sergiogarciainf@gmail.com>
2024-03-22 18:05:08 -0300

    Acrescentado a criação das tabelas no banco de dados H2

--- src/main/java/com/nelioalves/cursomc/domain/Categoria.java
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -2,10 +2,19 @@ package com.nelioalves.cursomc.domain;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+
+@Entity	
 public class Categoria implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	public Categoria(Integer id, String nome) {
